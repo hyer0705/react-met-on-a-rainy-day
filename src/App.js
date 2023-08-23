2a0691c5089fa21c66a807f7a98104b80f2f2eb8
hyer0705 <hyer1205@gmail.com>
2023-08-24 00:28:30 +0900

    use the effect cleanup function

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const clickCounter = () => {
-    setCounter((prev) => prev + 1);
-  };
-
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("항상 실행되는 코드!");
-
+function Hello() {
   useEffect(() => {
-    console.log("한 번만 실행되는 코드!");
+    console.log("hi :)");
+    return () => console.log("bye :("); // clean up 기능
   }, []);
-  useEffect(() => {
-    console.log("keyword가 변화될 때만 실행되는 코드!");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("counter가 변화될 때만 실행되는 코드!");
-  }, [counter]);
-  useEffect(() => {
-    console.log("keyword나 counter가 변했을 때 실행되는 코드!");
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="검색어를 입력해주세요."
-      />
-      <h1>{counter}</h1>
-      <button onClick={clickCounter}>Click me</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
