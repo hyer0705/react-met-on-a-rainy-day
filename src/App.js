3a12f71a5a735a1ccd3de1804250f721d5c21ddc
hyer0705 <hyer1205@gmail.com>
2023-08-24 00:06:13 +0900

    Use useEffect for counter, keyword state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const clickCounter = () => {
     setCounter((prev) => prev + 1);
   };
+
+  const onChange = (event) => setKeyword(event.target.value);
 
   console.log("항상 실행되는 코드!");
 
   useEffect(() => {
     console.log("한 번만 실행되는 코드!");
   }, []);
+  useEffect(() => {
+    console.log("keyword가 변화될 때만 실행되는 코드!");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("counter가 변화될 때만 실행되는 코드!");
+  }, [counter]);
+  useEffect(() => {
+    console.log("keyword나 counter가 변했을 때 실행되는 코드!");
+  }, [keyword, counter]);
 
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="검색어를 입력해주세요."
+      />
       <h1>{counter}</h1>
       <button onClick={clickCounter}>Click me</button>
     </div>
