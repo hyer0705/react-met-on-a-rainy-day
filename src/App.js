c6e03d2e734b937232a616f61a19dc0832d4e48b
hyer0705 <hyer1205@gmail.com>
2023-08-23 23:40:28 +0900

    Add counter button for studying effetcs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const clickCounter = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  console.log("rendered");
+
   return (
     <div>
-      <h1>Welcome Back!!!</h1>
+      <h1>{counter}</h1>
+      <button onClick={clickCounter}>Counter</button>
     </div>
   );
 }
