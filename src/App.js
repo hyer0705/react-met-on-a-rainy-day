2991838c100ec98baf4e1abece151e97d2f0af95
hyer0705 <hyer1205@gmail.com>
2023-08-26 16:53:55 +0900

    Add get coin info to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [showing, setShowing] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setShowing(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Welcome Back!!!</h1>
+      <h1>My Coins! {showing ? null : `(${coins.length})`}</h1>
+      {showing ? (
+        <h3>Loading...</h3>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
