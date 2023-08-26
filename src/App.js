f79aca4674a5e13ea6a3a12cf46dd45b0351c0ba
hyer0705 <hyer1205@gmail.com>
2023-08-26 16:05:12 +0900

    Add to do to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDos((currToDos) => [toDo, ...currToDos]);
+    setToDo("");
+  };
+
   return (
     <div>
-      <h1>Welcome Back!!!</h1>
+      <h1>My ToDos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          placeholder="할 일을 입력해주세요."
+          type="text"
+        ></input>
+        <button>추가</button>
+      </form>
     </div>
   );
 }
