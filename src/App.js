f9c45f67107b7c4798925ff715ca7098b6d7fb05
hyer0705 <hyer1205@gmail.com>
2023-08-28 11:29:49 +0900

    Add getting movie info to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const res = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+      )
+    ).json();
+
+    setMovies(res.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
-      <h1>Welcome Back!!!</h1>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <div className="wrapper__movies">
+          {movies.map((movie) => (
+            <div key={movie.id} className="wrapper__movie">
+              <img
+                className="movie__img"
+                src={movie.medium_cover_image}
+                alt={movie.title}
+              />
+              <h3 className="movie__title">{movie.title}</h3>
+              <p className="movie__summary">{movie.summary}</p>
+              <ul className="movie__genres">
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
