3cd39bde652dd7142ea475527cbb538ee49a28ab
hyer0705 <hyer1205@gmail.com>
2023-09-08 13:09:10 +0900

    Add Button Component to study moduled css and define propTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button.js";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome!!!</h1>
+      <h1 className={styles.title}>Welcome!!!</h1>
+      <Button text="Continue"></Button>
     </div>
   );
 }
