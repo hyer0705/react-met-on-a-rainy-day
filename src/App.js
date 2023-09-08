d168001174901066e31824c3bc7fc8b1df3329b7
hyer0705 <hyer1205@gmail.com>
2023-09-08 13:28:44 +0900

    Use useState to develop counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Button from "./Button.js";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+
   return (
     <div className="App">
-      <h1 className={styles.title}>Welcome!!!</h1>
-      <Button text="Continue"></Button>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text="click me" onClick={onClick}></Button>
     </div>
   );
 }
