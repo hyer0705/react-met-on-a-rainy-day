0187364d4828d3285bb64b179490862598839104
hyer0705 <hyer1205@gmail.com>
2023-09-08 14:15:11 +0900

    Use useEffect to study clean up func

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
-import Button from "./Button.js";
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => {
-    setCounter((prev) => prev + 1);
-  };
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("매번 실행!");
+function Hello() {
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("created:)");
+
+    return () => console.log("destroyed:(");
   }, []);
-  useEffect(() => {
-    console.log("I run when 'counter' changes...");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes...");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' and 'keyword' change");
-  }, [keyword, counter]);
+  return <h1>Hello!</h1>;
+}
+
+function App() {
+  const [hide, setHide] = useState(false);
+  const onClick = () => setHide((prev) => !prev);
 
   return (
     <div className="App">
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search keyword..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <Button text="click me" onClick={onClick}></Button>
+      {hide ? null : <Hello />}
+      <button onClick={onClick}>{hide ? "Show" : "Hide"}</button>
     </div>
   );
 }
