feec2c52bd79c29ab64822f9739ab815b8d9a3be
hyer0705 <hyer1205@gmail.com>
2023-09-08 13:51:08 +0900

    Use Deps to study when code is executed

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  console.log("매번 실행!");
-  useEffect(() => {
-    console.log("CALL THE API...");
-  }, []);
+  const [keyword, setKeyword] = useState("");
 
   const onClick = () => {
     setCounter((prev) => prev + 1);
   };
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("매번 실행!");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'counter' changes...");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes...");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' and 'keyword' change");
+  }, [keyword, counter]);
 
   return (
     <div className="App">
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search keyword..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button text="click me" onClick={onClick}></Button>
     </div>
