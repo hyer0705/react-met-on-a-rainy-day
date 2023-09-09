d93363cc6e764f9f581436085fcd9068144745e2
hyer0705 <hyer1205@gmail.com>
2023-09-10 05:28:36 +0900

    Use Gecko API to get coin list

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const getCoins = async () => {
+    const res = await (
+      await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`
+      )
+    ).json();
 
-    if (toDo === "") {
-      return;
-    }
-
-    setToDos((currTodos) => [toDo, ...currTodos]);
-    setToDo("");
+    setCoins(res);
   };
+  useEffect(() => {
+    getCoins();
+    setLoading(false);
+  }, []);
 
   return (
     <div className="App">
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="할 일을 입력해주세요."
-          type="text"
-        />
-        <button>추가</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((toDo, idx) => (
-          <li key={idx}>{toDo}</li>
-        ))}
-      </ul>
+      <span>
+        Data provided by <a href="https://www.coingecko.com/">CoinGecko</a>
+      </span>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
     </div>
   );
 }
