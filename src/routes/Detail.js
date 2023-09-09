69ad08dc556e045090472598794cf4d94e0fa799
hyer0705 <hyer1205@gmail.com>
2023-09-10 07:35:07 +0900

    Use useParams() to get movie id

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const getMovie = async () => {
+    const res = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+
+    setMovie(res.data.movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <h1>{movie.title}</h1>
+          <div>
+            <span>{movie.year} </span>
+            <span>⭐{movie.rating}</span>
+          </div>
+          <p>{movie.description_intro}</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
