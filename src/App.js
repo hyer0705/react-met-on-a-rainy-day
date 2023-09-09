40a03aba1fddb9178103db420a3710f54986ab2f
hyer0705 <hyer1205@gmail.com>
2023-09-10 04:21:02 +0900

    Use spread syntax to set ToDos Array

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created:)");
-
-    return () => console.log("destroyed:(");
-  }, []);
-  return <h1>Hello!</h1>;
-}
-
 function App() {
-  const [hide, setHide] = useState(false);
-  const onClick = () => setHide((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if (toDo === "") {
+      return;
+    }
+
+    setToDos((currTodos) => [toDo, ...currTodos]);
+    setToDo("");
+  };
 
   return (
     <div className="App">
-      {hide ? null : <Hello />}
-      <button onClick={onClick}>{hide ? "Show" : "Hide"}</button>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          placeholder="할 일을 입력해주세요."
+          type="text"
+        />
+        <button>추가</button>
+      </form>
     </div>
   );
 }
