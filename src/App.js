97c17dd1f0c9355353407449afa3402775824e9f
hyer0705 <hyer1205@gmail.com>
2023-09-10 07:16:01 +0900

    Use react-router-dom to develop routing fn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home.js";
+import Detail from "./routes/Detail.js";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
